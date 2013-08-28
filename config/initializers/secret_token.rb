e7e9f401bf09a235b2b8f0f15384e639716b8d7c
Yitao Sun <yitaosun@gmail.com>
2013-08-29 01:34:51 +0800

    Fresh project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Codecamp::Application.config.secret_key_base = '27eb55d7a6495f6412a5d01e414b096e0f5b27298d98af3fc00b4eb0a81405e69e91e75fbdeeff85ed02e70cd00c1524bb7e135137095e2ca0b2ceae6a365514'
